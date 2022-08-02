23a536bfc860903d9f72634073e423a7007f0364
LeonOfc <110469920+LeonOfc@users.noreply.github.com>
2022-08-03 00:30:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-let fs = require('fs')
+Alet fs = require('fs')
 global.owner = JSON.parse(fs.readFileSync('./src/owner.json')) // Put your number to /src/owner.json
 global.mods = JSON.parse(fs.readFileSync('./src/moderator.json')) // Want some help?
 global.thumbt = global.img
 global.fsx = 999999999999
 global.kontak = [
-['62895336282144', '𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷', true],
-['6282140373458', 'Raxcel-MD ⁴⁰⁴-ﾉ [BOT MD]', true]
+['6287760550924', 'Lord Md', true],
+['6287760550924', 'Lord-MD ⁴⁰⁴-ﾉ [BOT MD]', true]
 ]
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -34,12 +34,12 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
-global.WM = '𝑴𝒚𝑩𝒐𝒕-𝑴𝒖𝒍𝒕𝒊 𝑫𝒆𝒗𝒊𝒄𝒆 𝑩𝒚 𝚃𝚑𝚎.𝚂𝚊𝚍.𝙱𝚘𝚢𝟶𝟷'
-global.linkgc = 'https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP'
-global.linkig = 'https://www.instagram.com/the.sad.boy01'
-global.deslink = ''
+global.WM = 'LeonOfc👑'
+global.linkgc = 'https://chat.whatsapp.com/CLW59KymKua8IwPrXT8ZYj'
+global.linkig = 'gada-ig'
+global.deslink = 'github.com/LeonOfc'
 global.titlink = 'Folllow me on Instagram'
-global.bodlink = '@the.sad.boy01'
+global.bodlink = '@LeonBotz'
 
 global.wait = '_*𝐬𝐞𝐝𝐚𝐧𝐠 𝐝𝐢 𝐩𝐫𝐨𝐬𝐞𝐬 ...*_'
 global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
@@ -49,8 +49,8 @@ global.dtu = '𝑭𝒐𝒍𝒍𝒐𝒘'
 global.urlnya = "https://www.instagram.com/the.sad.boy01"
 
 //============= callButtons ============//
-global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '+62 895-3362-82144'
+global.dtc = 'Lord Leon'
+global.phn = '6287760550924'
 
 //========== Tampilan Bot =============//
 global.sa = '╭─'
